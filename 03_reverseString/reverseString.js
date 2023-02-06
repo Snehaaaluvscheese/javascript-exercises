e85943bd2bbd90a85cf8c392506af435e0caaaea
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-02-07 00:41:16 +0530

    Add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+const myArray = text.split("");
+myArray.reverse();
+let reversedString = myArray.join("");
+return reversedString;
 };
 
 // Do not edit below this line
