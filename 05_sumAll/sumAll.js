dd67bced2803b29e2fd68fc65508e292604962cf
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-02-09 10:00:49 +0530

    Add function sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+ let result;
+ if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+    result = "ERROR";
+    return result;
+ } else if (b > a) {
+    result = (((b - a + 1)/2) * (a+b));
+    return result;
+ } else if (a > b) {
+    result = (((a - b + 1)/2) * (b+a));
+    return result;
+ } 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
