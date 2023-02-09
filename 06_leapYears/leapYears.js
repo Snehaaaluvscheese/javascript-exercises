efdfa4274b623bd36b0614881813d4ca7f4f14b5
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-02-09 13:34:21 +0530

    Add function leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(a) {
+    if (a % 4 === 0) {
+     if (a  % 100 === 0 && a % 400 === 0) {
+      return true
+       } else if (a  % 100 === 0 && a % 400 !== 0) {
+       return false
+        } else { return true }
+      }
+    else { return false }
+    };
 
 // Do not edit below this line
 module.exports = leapYears;
