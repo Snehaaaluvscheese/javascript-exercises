a5b3602a5b1c9e3204c31bbd12739a294359ff4b
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-02-10 02:10:42 +0530

    Add functions convertToCelsius and convertToFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let calculation =  (temp - 32) * 5/9
+  let result = Math.round(calculation * 10) / 10
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let calculation2 = (temp2 * 9/5 + 32)
+  let result2 = Math.round(calculation2 * 10) / 10
+  return result2;
 };
 
 // Do not edit below this line
