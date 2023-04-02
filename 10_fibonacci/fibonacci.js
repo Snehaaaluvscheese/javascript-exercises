714541f3e0a83a5df378a9c988fd8b6189fcdc94
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-04-03 03:10:02 +0530

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  
+    if (number < 0) {
+        return "OOPS";
+    } else if (number > 0) {
+        
+      let n1 = 0;
+      let n2 = 1;
+      let nextTerm;
+      let myArray = [];
+
+      for (let i = 1; i <= number; i++) {
+      
+       nextTerm = n1 + n2;
+       myArray.push(nextTerm);
+       n1 = n2;
+       n2 = nextTerm;
+  }
+  myArray.unshift(1);
+  let position = number - 1;
+
+  return (myArray[position]);
+   
+    
+    
+    
+  }
 
 };
 
