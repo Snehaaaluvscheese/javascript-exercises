0569f0eebc2e2d090276ba1711710bce5ee8c91c
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-04-03 03:09:19 +0530

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,30 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  if (typeof word === "string") {
+    const word2 = word.toLowerCase();
+    const myArray = word2.split("");
+    let myArray2 = myArray.filter(checkLetter);
+    
+
+    function checkLetter(element) {
+        return element.length === 1 && element.match(/[a-z]/i);
+    }
+
+    
+     
+    const myArray2Back = [].concat(myArray2).reverse();
+    
+    let word1 = myArray2.join("");
+    let word3 = myArray2Back.join("");
+
+
+    if (word1 === word3) {
+        return true;
+    } else {
+        return false;
+    }
+  } else {
+    return false;
+  }
 
 };
 
